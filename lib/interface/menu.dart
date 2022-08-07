be3b0cb2849d7de8516078d9bb23cb4c344af11a
Bundit Nuntates <silkyland@gmail.com>
2022-08-07 15:56:52 +0700

    scrafold

---
+++ lib/interface/menu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Menu {
+  final Icon icon;
+  final String title;
+  final Widget screen;
+
+  Menu({
+    required this.icon,
+    required this.title,
+    required this.screen,
+  });
+}
